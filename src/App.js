d404ab18d4e2f4e7aa2a850df5368aee7ba501e8
First <first@Sarawut-Macbook.local>
2023-04-30 23:14:11 +0700

    list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
 function App() {
-  return <div className="w-screen h-screen flex justify-center items-center">Hello World</div>;
+  const Cards = ({ title }) => (
+    <div
+      role="button"
+      className="w-[300px] h-[100px] shadow-md hover:shadow-white text-white flex justify-center rounded-lg select-none cursor-pointer active:shadow-inner"
+      children={title}
+    />
+  );
+  const topic = [
+    "components",
+    "props",
+    "Import & Export",
+    "Function & Trigger",
+    "Event Target",
+    "useState",
+    "rerender effect",
+    "Promise object",
+    "Resolve Reject",
+    "then catch",
+    "async await",
+    "try catch",
+    "error object",
+    "fetch",
+    "axios",
+    "useEffect",
+    "useContext",
+  ];
+  return (
+    <div className="w-screen h-screen flex flex-col item-center items-center gap-4 bg-slate-700 py-20 ">
+      {topic.map((i, d) => (
+        <Cards key={d} title={i} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
